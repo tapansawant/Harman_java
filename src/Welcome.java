bc6b1e94be461a79667821fa8b54c686460f7589
tapansawant <tapansawant45@gmail.com>
2022-03-14 17:16:56 +0530

    hello wld completed

---
+++ src/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+
+    public static void main(String[] args) {
+
+        Integer a = 10, b = 12, c;
+        c = a+b;
+       // String name = "Tapan";
+        System.out.println(c);
+    }
+}
